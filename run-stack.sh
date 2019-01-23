5ec0e1ec111118efbebaf7dea3d820ff8bc130ef
Allan Eayrs <ae.eayrs@gmail.com>
2019-01-23 15:19:15 -0800

    Updated load balancer arn

--- run-stack.sh
+++ run-stack.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 aws cloudformation $ACTION \
     --region us-west-2 \
@@ -9,4 +9,4 @@ aws cloudformation $ACTION \
     ParameterKey=DockerImage,ParameterValue=801420687740.dkr.ecr.us-west-2.amazonaws.com/example-webapp:$(git rev-parse HEAD) \
     ParameterKey=VPC,ParameterValue=vpc-a42d01c3 \
     ParameterKey=Cluster,ParameterValue=default \
-    ParameterKey=Listener,ParameterValue=arn:aws:elasticloadbalancing:us-west-2:801420687740:listener/app/production-website/fcd624047255bcfd/3e5588825593ea58
+    ParameterKey=Listener,ParameterValue=arn:aws:elasticloadbalancing:us-west-2:801420687740:listener/app/production-website/06a9810e80594273/880d4a5dde5901b9
