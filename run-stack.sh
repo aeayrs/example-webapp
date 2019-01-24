14363a4590f397e480bf1c599a2dbe7bfa7d0217
Allan Eayrs <ae.eayrs@gmail.com>
2019-01-24 08:56:54 -0800

    Added ALB_LISTENER_ARN parameter

--- run-stack.sh
+++ run-stack.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
-
+#Production ALB_LISTENER_ARN=arn:aws:elasticloadbalancing:us-west-2:801420687740:listener/app/production-website/efb4a88454b15956/34c95ff0a6dc4e10
 aws cloudformation $ACTION \
     --region us-west-2 \
     --stack-name $STACK_NAME \
@@ -9,4 +9,4 @@ aws cloudformation $ACTION \
     ParameterKey=DockerImage,ParameterValue=801420687740.dkr.ecr.us-west-2.amazonaws.com/example-webapp:$(git rev-parse HEAD) \
     ParameterKey=VPC,ParameterValue=vpc-a42d01c3 \
     ParameterKey=Cluster,ParameterValue=larger-cluster \
-    ParameterKey=Listener,ParameterValue=arn:aws:elasticloadbalancing:us-west-2:801420687740:listener/app/production-website/efb4a88454b15956/34c95ff0a6dc4e10
+    ParameterKey=Listener,ParameterValue=$ALB_LISTENER_ARN
