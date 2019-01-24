9693eb672ddb27be0b2a37c263e49dafb14c186e
Allan Eayrs <ae.eayrs@gmail.com>
2019-01-23 23:03:53 -0800

    Changed Cluster to larger-cluster

--- run-stack.sh
+++ run-stack.sh
@@ -8,5 +8,5 @@ aws cloudformation $ACTION \
     --parameters \
     ParameterKey=DockerImage,ParameterValue=801420687740.dkr.ecr.us-west-2.amazonaws.com/example-webapp:$(git rev-parse HEAD) \
     ParameterKey=VPC,ParameterValue=vpc-a42d01c3 \
-    ParameterKey=Cluster,ParameterValue=my-next-cluster \
+    ParameterKey=Cluster,ParameterValue=larger-cluster \
     ParameterKey=Listener,ParameterValue=arn:aws:elasticloadbalancing:us-west-2:801420687740:listener/app/production-website/efb4a88454b15956/34c95ff0a6dc4e10
